c2933b7be2356ca1306203a4d4a37632566848cf
marcusSystementor <144769052+marcusSystementor@users.noreply.github.com>
2023-09-18 14:08:16 +0200

    Upload

---
+++ JavaWhileTo10.java
@@ -0,0 +1,15 @@
+public class Java_Count_10 {
+
+    public static void main(String[] args) {
+        
+        int x = 1;
+
+        while(x <= 10) {
+            System.out.println(x);
+            x++;
+
+        }
+
+    }
+    
+}
